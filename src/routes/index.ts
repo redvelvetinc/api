b56f0442dda8edc4a90137a4b5a13ca4cdc002dc
Rafaell Lycan <sonny.webdsg@gmail.com>
2018-06-28 11:48:45 +0200

    Change API routes folder

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { version } from '../../package.json';
+import { env } from 'process';
 
 class Root {
   public router: Router
@@ -11,15 +12,22 @@ class Root {
 
   private initialize(): void {
     this.router.get('/', this.index);
+    this.router.get('/metrics/health', this.health);
   }
 
-  public index(req: Request, res: Response, next: NextFunction) {
-    res.json({
+  public index(req: Request, res: Response) {
+    res.status(200).json({
       message: 'Cabaret API - Red Velvet Inc.',
-      status: 'online',
+      status: 'OK',
       version
+    });
+  }
+
+  public health(req: Request, res: Response) {
+    res.status(200).json({
+      status: 'OK'
     });
   }
 }
 
-export default new Root();
+export default new Root().router;
